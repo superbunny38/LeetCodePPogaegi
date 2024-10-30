42554b5110341d2a66b550d3b88fe650604d1aaf
Chaeeun Ryu <48243487+superbunny38@users.noreply.github.com>
2024-10-30 14:59:13 -0400

    Update 55.py -- Jump Game
    이렇게 푸는거였군..

--- week1/ch/55.py
+++ week1/ch/55.py
@@ -1,33 +1,8 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        dp = [False]*len(nums)
-        if len(nums) == 1:
-            return True
-        
-        if len(nums) == 2:
-            if nums[0]>0:
-                return True
-            else:
-                return False
-        
-        if nums[0] == 0:
-            return False
-        else:
-            dp[0] = True
-
-        for idx, avail_step in enumerate(nums):
-            if dp[idx]:
-                if avail_step == 0:
-                    continue
-                else:
-                    for take_step in range(1,avail_step+1):
-                        if take_step+idx<len(dp):
-                            dp[take_step+idx]=True
-                            if take_step+idx == len(dp)-1:
-                                return True
-                        else:
-                            break
-            else:
-                return False
-
-        return dp[-1]
+        last_idx = len(nums)-1
+        min_reachable = last_idx
+        for idx in range(len(nums)-2,-1,-1):
+            if idx+nums[idx]>=min_reachable:
+                min_reachable = idx
+        return min_reachable == 0
