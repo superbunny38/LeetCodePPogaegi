ba479c62e77df869810650773937879f330f0500
Chaeeun Ryu <48243487+superbunny38@users.noreply.github.com>
2024-11-08 22:18:18 -0500

    Update 435.py 정석 84/82

--- week2/ch/435.py
+++ week2/ch/435.py
@@ -1,22 +1,12 @@
 class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
         intervals.sort(key=lambda x:x[1])
-        print("intervals:",intervals)
-        def merge(interval1,interval2):
-            return min(interval1[0],interval2[0]),max(interval1[1],interval2[1])
-
-        ret = []
-        change_count = 0
-        for interval in intervals:
-            if len(ret) == 0:
-                ret.append(interval)
-            else:
-                last_elem = ret[-1][1]
-                if interval[0]<last_elem:
-                    print("remove:",interval)
-                    change_count+=1
-                    continue
-                else:
-                    ret.append(interval)
+        n_erase = 0
+        prev_idx = 0
+        for idx in range(1,len(intervals)):
+            if intervals[prev_idx][1]>intervals[idx][0]:
+                n_erase += 1
                 
-        return change_count
+            else:
+                prev_idx = idx
+        return n_erase
